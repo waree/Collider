7ca177f636d3e996caba41dfb65f45fe255febcd
waree <norbi.szilagyi95@gmail.com>
2015-06-28 16:28:31 +0200

    T

--- js/collider.js
+++ js/collider.js
@@ -84,24 +84,8 @@ $("#board")
 		}
 	});
 	
-var lastCalledTime;
-var fps;	
-	
 function render() {
 	if (!gameover && !gamestart) {	
-		if(!lastCalledTime) {
-			 lastCalledTime = Date.now();
-			 fps = 0;
-			 window.requestAnimFrame(render);
-			 return;
-		  }
-		  
-		 delta = (new Date().getTime() - lastCalledTime)/1000;
-		  lastCalledTime = Date.now();
-		  fps = 1/delta; 
-		  
-		 $("#points").html(Math.floor(fps));
-	
 		c.clearRect(0, 0, b.height, b.width);
 		p.drawBoardBalls();	
 		window.requestAnimFrame(render);	
